3b7aee1b78f1febec2f14f995f5bed4aa2580231
JunXiangCai <1092925421@qq.com>
2021-01-28 21:43:48 +0800

    循环守卫

---
+++ src/main/scala/pers/caijx/chapter04/myfor/ForGuard.scala
@@ -0,0 +1,12 @@
+package pers.caijx.chapter04.myfor
+
+object ForGuard {
+
+  def main(args: Array[String]): Unit = {
+    for (i <- 1 to 3 if i != 2) {
+      print(i + " ")
+    }
+    println()
+  }
+
+}
